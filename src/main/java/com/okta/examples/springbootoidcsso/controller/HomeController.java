b1a87ad27c3c590d77c64629ada0e35991139f75
Micah Silverman <micah@afitnerd.com>
2019-09-27 01:00:18 -0500

    Added codemirror and claims formatting

--- src/main/java/com/okta/examples/springbootoidcsso/controller/HomeController.java
+++ src/main/java/com/okta/examples/springbootoidcsso/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.okta.examples.springbootoidcsso.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
@@ -9,11 +11,17 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 
+    ObjectMapper mapper = new ObjectMapper();
+
     @GetMapping("/")
-    public ModelAndView home(@AuthenticationPrincipal OidcUser user) {
+    public ModelAndView home(@AuthenticationPrincipal OidcUser user) throws JsonProcessingException {
         ModelAndView mav = new ModelAndView();
         mav.addObject("user", user.getUserInfo());
         mav.addObject("idToken", user.getIdToken().getTokenValue());
+        mav.addObject(
+            "claims",
+            mapper.writerWithDefaultPrettyPrinter().writeValueAsString(user.getClaims())
+        );
         mav.setViewName("home");
         return mav;
     }
